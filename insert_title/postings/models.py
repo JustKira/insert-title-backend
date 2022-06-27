2fbb16b255cc174b6981a0e2c4fbf37150682192
Yousef Radwan <54478004+yradwan147@users.noreply.github.com>
2022-06-27 16:39:43 +0200

    add #3

--- insert_title/postings/models.py
+++ insert_title/postings/models.py
@@ -5,8 +5,4 @@ from django.db import models
 class Posting1(models.Model):
     name = models.CharField(max_length=200)
     name2 = models.CharField(max_length=200)
-<<<<<<< HEAD
     name3 = models.CharField(max_length=300)
-=======
-
->>>>>>> e5225c111ab975d1e3057b6fd3b1255af7f86523
