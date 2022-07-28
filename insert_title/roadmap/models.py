96b2696ffb0b6d699e370141247d3931752127a4
mohamed tarek <mt1348@fayoum.edu.eg>
2022-07-28 22:43:04 +0200

    last changes

--- insert_title/roadmap/models.py
+++ insert_title/roadmap/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Roadmap(models.Model):
     road_map_name = models.CharField(max_length=250)
     node_name=models.CharField(max_length=250)
-    nested_nodes = models.PositiveIntegerField(default=0)
+    nested_nodes = models.CharField(default=0,max_length=250)
     finished_node=models.BooleanField(default=False)
 
 
